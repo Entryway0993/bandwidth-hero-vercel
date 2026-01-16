299dc77c75c8c7263c3f852c56bd3346f488b99e
Entryway0993 <247029828+Entryway0993@users.noreply.github.com>
2026-01-16 03:12:27 +0000

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -124,9 +124,9 @@ function clamp(v, min, max) {
 function optimizeAvifParams(width, height) {
   const area = width * height;
   if (area > LARGE_IMAGE_THRESHOLD)
-    return { tileRows: 1, tileCols: 1, minQuantizer: 20, maxQuantizer: 40, effort: 3 };
+    return { tileRows: 4, tileCols: 4, minQuantizer: 20, maxQuantizer: 40, effort: 3 };
   if (area > MEDIUM_IMAGE_THRESHOLD)
-    return { tileRows: 1, tileCols: 1, minQuantizer: 28, maxQuantizer: 48, effort: 3 };
+    return { tileRows: 2, tileCols: 2, minQuantizer: 28, maxQuantizer: 48, effort: 3 };
   return { tileRows: 1, tileCols: 1, minQuantizer: 26, maxQuantizer: 46, effort: 4 };
 }
 
@@ -166,6 +166,7 @@ function fail(message, req, res, err = null) {
   }));
   redirect(req, res);
 }
+
 
 
 
